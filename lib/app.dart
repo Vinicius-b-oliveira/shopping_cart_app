f17f5ac44d091e3f3c7c67b8d88036613a8925e3
Vini <viniciusbuenodeoliveira2017@gmail.com>
2025-05-16 14:53:15 -0300

    Criando o tema do app

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_cart_app/core/theme/app_theme.dart';
 
 class ShoppingApp extends StatelessWidget {
   const ShoppingApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Shopping Cart App',
+      theme: AppTheme.darkTheme,
+    );
   }
 }
---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final Color backgroundColor = Color(0xFF2B273D);
+  static final Color primaryColor = Color(0xFFEFB92B);
+  static final Color textColor = Color(0xFFEAEAEA);
+
+  static ThemeData get darkTheme {
+    final ColorScheme colorScheme = ColorScheme.fromSeed(
+      seedColor: primaryColor,
+      brightness: Brightness.dark,
+      primary: primaryColor,
+      onPrimary: backgroundColor,
+      secondary: primaryColor.withAlpha(204),
+      onSecondary: textColor,
+      surface: backgroundColor,
+      onSurface: textColor,
+    );
+
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: colorScheme,
+      appBarTheme: AppBarTheme(centerTitle: true),
+    );
+  }
+}
