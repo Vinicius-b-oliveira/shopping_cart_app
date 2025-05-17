5ba6c9a5853d941f9e7e8dd56cc78c1a36f8aeb1
Vini <viniciusbuenodeoliveira2017@gmail.com>
2025-05-17 14:54:06 -0300

    Refatoriando viewmodel para lidar melhor com o estado de loading e requisições em background

--- lib/presentation/viewmodel/shopping_viewmodel.dart
+++ lib/presentation/viewmodel/shopping_viewmodel.dart
@@ -27,10 +27,15 @@ class ShoppingViewModel extends _$ShoppingViewModel {
   Future<void> addItem(String name) async {
     if (name.trim().isEmpty) return;
 
-    state = const AsyncValue.loading();
+    state = AsyncValue<List<ShoppingItem>>.loading().copyWithPrevious(state);
+
     try {
-      await _repository.addItem(name.trim());
-      state = await AsyncValue.guard(_fetchItems);
+      final newItem = await _repository.addItem(name.trim());
+
+      final currentList = state.valueOrNull ?? [];
+      final updatedList = [newItem, ...currentList];
+
+      state = AsyncValue.data(updatedList);
     } catch (e) {
       state = AsyncValue.error(e, StackTrace.current);
     }
@@ -48,28 +53,41 @@ class ShoppingViewModel extends _$ShoppingViewModel {
 
     state = AsyncValue.data(updatedList);
 
-    await _repository.togglePurchasedStatus(item.id, item.isPurchased);
+    try {
+      await _repository.togglePurchasedStatus(item.id, item.isPurchased);
+    } catch (e) {
+      state = await AsyncValue.guard(_fetchItems);
+    }
   }
 
   Future<void> updateItemName(ShoppingItem item, String newName) async {
     if (newName.trim().isEmpty) return;
 
-    state = const AsyncValue.loading();
+    final currentList = state.value ?? [];
+    final updatedList =
+        currentList
+            .map((i) => i.id == item.id ? i.copyWith(name: newName.trim()) : i)
+            .toList();
+
+    state = AsyncValue.data(updatedList);
+
     try {
       await _repository.updateItemName(item.id, newName.trim());
-      state = await AsyncValue.guard(_fetchItems);
     } catch (e) {
-      state = AsyncValue.error(e, StackTrace.current);
+      state = await AsyncValue.guard(_fetchItems);
     }
   }
 
   Future<void> deleteItem(String id) async {
-    state = const AsyncValue.loading();
+    final currentList = state.value ?? [];
+    final updatedList = currentList.where((item) => item.id != id).toList();
+
+    state = AsyncValue.data(updatedList);
+
     try {
       await _repository.deleteItem(id);
-      state = await AsyncValue.guard(_fetchItems);
     } catch (e) {
-      state = AsyncValue.error(e, StackTrace.current);
+      state = await AsyncValue.guard(_fetchItems);
     }
   }
 }
